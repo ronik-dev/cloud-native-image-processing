d4441855c6ff4b0c107ac61667cfefa2efd6b07b
Romano Nicola <nicola.romano@student.supsi.ch>
2026-05-28 18:31:20 +0000

    task(#1): initialize Spring Boot project with dependencies

---
+++ src/main/java/ch/supsi/imageprocessing/CloudNativeImageProcessingApplication.java
@@ -0,0 +1,13 @@
+package ch.supsi.imageprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNativeImageProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNativeImageProcessingApplication.class, args);
+	}
+
+}

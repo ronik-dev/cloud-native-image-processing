41fe21440e8a370a49f3f7c9100955ab9bd9c44b
nicola romano <nicola.romano@student.supsi.ch>
2026-06-09 20:24:05 +0200

    copy image

--- src/main/java/ch/supsi/imageprocessing/processor/ImageProcessor.java
+++ src/main/java/ch/supsi/imageprocessing/processor/ImageProcessor.java
@@ -16,6 +16,7 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.UUID;
 
 @Component
@@ -43,12 +44,13 @@ public class ImageProcessor {
 				Path tempOutputFile = Files.createTempFile("ffmpeg-output-"+UUID.randomUUID().toString(), "." + job.getTargetFormat());
 
 				try {
-						switch(job.getType()){
+						switch (job.getType()) {
 								case JobType.FORMAT_CONVERSION:
 										ffmpegConvert(sourceFile, tempOutputFile);
 										break;
 								case JobType.BACKGROUND_REMOVAL:
 										Thread.sleep(10000);
+										Files.copy(sourceFile.toPath(), tempOutputFile, StandardCopyOption.REPLACE_EXISTING);
 										break;
 								default:
 										throw new UnsupportedOperationException("Job type " + job.getType() + " is not yet implemented.");
@@ -56,7 +58,6 @@ public class ImageProcessor {
 						try (InputStream is = Files.newInputStream(tempOutputFile)) {
 								ss.storeLocalFile(is, job.getTargetStorageKey());
 						}
-
 				} finally {
 						Files.deleteIfExists(tempOutputFile);
 				}
@@ -66,11 +67,11 @@ public class ImageProcessor {
 
 		private void ffmpegConvert(File input, Path output) throws IOException, InterruptedException {
 				ProcessBuilder pb = new ProcessBuilder(
-						"ffmpeg",
-						"-y", 
-						"-i", input.getAbsolutePath(),
-						output.toAbsolutePath().toString()
-				);
+								"ffmpeg",
+								"-y", 
+								"-i", input.getAbsolutePath(),
+								output.toAbsolutePath().toString()
+								);
 
 				pb.redirectErrorStream(true);
 				pb.redirectOutput(ProcessBuilder.Redirect.DISCARD);

594a5f1778ed3bfa334d087100fc020eda70271d
nicola romano <nicola.romano@student.supsi.ch>
2026-08-20 17:40:45 +0200

    missing kafka annotation fix

--- orchestrator/src/main/java/ch/supsi/imageprocessing/config/KafkaConsumerConfig.java
+++ orchestrator/src/main/java/ch/supsi/imageprocessing/config/KafkaConsumerConfig.java
@@ -5,6 +5,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
@@ -12,6 +13,7 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
+@EnableKafka
 @Configuration
 public class KafkaConsumerConfig {
 

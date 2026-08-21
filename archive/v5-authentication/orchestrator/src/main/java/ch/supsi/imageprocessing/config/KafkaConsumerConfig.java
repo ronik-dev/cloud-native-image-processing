cb82eeec03db0d0f11a97873d003adbd081674b4
nicola romano <nicola.romano@student.supsi.ch>
2026-08-21 11:27:18 +0200

    brought up to date v4 and v5

--- archive/v5-authentication/orchestrator/src/main/java/ch/supsi/imageprocessing/config/KafkaConsumerConfig.java
+++ archive/v5-authentication/orchestrator/src/main/java/ch/supsi/imageprocessing/config/KafkaConsumerConfig.java
@@ -9,6 +9,8 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.converter.RecordMessageConverter;
+import org.springframework.kafka.support.converter.StringJacksonJsonMessageConverter;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -30,10 +32,16 @@ public class KafkaConsumerConfig {
     }
 
     @Bean
+    public RecordMessageConverter converter() {
+        return new StringJacksonJsonMessageConverter();
+    }
+
+    @Bean
     public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(
-            ConsumerFactory<String, String> consumerFactory) {
+            ConsumerFactory<String, String> consumerFactory, RecordMessageConverter converter) {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
+        factory.setRecordMessageConverter(converter);
         return factory;
     }
 }
